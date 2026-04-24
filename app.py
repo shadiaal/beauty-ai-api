2c9b8f7e086a9e71b1a806697bafc543b9850f62
shadiaal <shadiaalmtiri@gmail.com>
2026-04-24 18:18:54 +0300

    fix render port binding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -52,4 +53,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
